b4e284d72ae547f8e280ea67cf6346329652e36e
Filipedss123 <61808894+Filipedss123@users.noreply.github.com>
2020-06-15 22:04:38 -0300

    arquivos html e php para formulario

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mensagem Form</title>
+</head>
+<style>
+    body{
+        margin:0px;
+        padding :0px;
+        background-color: rgba(164, 58, 226, 0.5);
+        text-align:center;
+        margin-top:400px;
+    }
+    
+</style>
+<body>
+<?php
+echo "<h1>Obrigado Pelo Contato.</h1>";
+?>
+<?php
+echo "<h2>Em breve Retornaremos seu contato.</h2>";
+?>
+</body>
+</html>
+
+
+
